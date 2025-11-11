66e257379dfbd1787a1b07f8d8033403086ece0a
Marcel <kaliqmarcel@gmail.com>
2025-11-11 14:59:39 -0300

    indexação login

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Pages/Home/index.jsx'
+import Login from './Pages/Login/login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
