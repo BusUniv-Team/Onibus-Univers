5b3a15c0fb78221b21c5c7072aeb5156384207d4
Marcel <kaliqmarcel@gmail.com>
2025-11-12 14:54:30 -0300

    Pagina de Login1

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Pages/Home/index.jsx'
 import Login from './Pages/Login/login.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
